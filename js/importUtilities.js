c6572ce559e1d888b029478fd48e71b39c40293b
Mayank <mayankgupta8900@gmail.com>
2021-11-23 12:43:21 +0530

    file name and indentation

--- js/importUtilities.js
+++ js/importUtilities.js
@@ -1,5 +1,5 @@
 $(function () {
-    $("#footer").load("utilities/footer.html");
-    $('#header').load("utilities/header.html");
-    $('#gradiant-box').load("utilities/gradiantBox.html")
+  $("#footer").load("utilities/footer.html");
+  $('#header').load("utilities/header.html");
+  $('#gradiant-box').load("utilities/gradiantBox.html")
 });
--- js/validation.js
+++ js/validation.js
@@ -1,35 +1,35 @@
 function func() {
-    let x = document.forms["form"]["name"].value;
-    let y = document.forms["form"]["email"].value;
-    let z = document.forms["form"]["pno"].value;
-    let a = document.forms["form"]["query"].value;
-    let text;
-    if (x == "" || x.length < 3 || !isNaN(x)) {
-        document.getElementById("isFormValid").classList.add("alert-danger");
-        document.getElementById("name").classList.add("is-invalid");
-        text = "Enter the valid name";
-    } else if (y == "") {
-        document.getElementById("isFormValid").classList.add("alert-danger");
-        document.getElementById("email").classList.add("is-invalid");
-        text = "Enter the form Completely";
-    } else if (z == "" || isNaN(z) || z[0] < 6 || z.length < 10 || z.length > 10) {
-        document.getElementById("isFormValid").classList.add("alert-danger");
-        document.getElementById("phone").classList.add("is-invalid");
-        text = "Enter the valid number";
-    } else if (a == "") {
-        document.getElementById("isFormValid").classList.add("alert-warning");
-        text = "Please Type Your Query!!";
-    } else {
-        document.getElementById("isFormValid").classList.remove("alert-danger");
-        document.getElementById("name").classList.remove("is-invalid");
-        document.getElementById("email").classList.remove("is-invalid");
-        document.getElementById("phone").classList.remove("is-invalid");
-        document.getElementById("name").classList.add("is-valid");
-        document.getElementById("email").classList.add("is-valid");
-        document.getElementById("phone").classList.add("is-valid");
-        document.getElementById("isFormValid").classList.add("alert-success");
-        text = "All good";
-    }
-    document.getElementById("isFormValid").innerHTML = text;
-    return x;
+  let x = document.forms["form"]["name"].value;
+  let y = document.forms["form"]["email"].value;
+  let z = document.forms["form"]["pno"].value;
+  let a = document.forms["form"]["query"].value;
+  let text;
+  if (x == "" || x.length < 3 || !isNaN(x)) {
+    document.getElementById("isFormValid").classList.add("alert-danger");
+    document.getElementById("name").classList.add("is-invalid");
+    text = "Enter the valid name";
+  } else if (y == "") {
+    document.getElementById("isFormValid").classList.add("alert-danger");
+    document.getElementById("email").classList.add("is-invalid");
+    text = "Enter the form Completely";
+  } else if (z == "" || isNaN(z) || z[0] < 6 || z.length < 10 || z.length > 10) {
+    document.getElementById("isFormValid").classList.add("alert-danger");
+    document.getElementById("phone").classList.add("is-invalid");
+    text = "Enter the valid number";
+  } else if (a == "") {
+    document.getElementById("isFormValid").classList.add("alert-warning");
+    text = "Please Type Your Query!!";
+  } else {
+    document.getElementById("isFormValid").classList.remove("alert-danger");
+    document.getElementById("name").classList.remove("is-invalid");
+    document.getElementById("email").classList.remove("is-invalid");
+    document.getElementById("phone").classList.remove("is-invalid");
+    document.getElementById("name").classList.add("is-valid");
+    document.getElementById("email").classList.add("is-valid");
+    document.getElementById("phone").classList.add("is-valid");
+    document.getElementById("isFormValid").classList.add("alert-success");
+    text = "All good";
+  }
+  document.getElementById("isFormValid").innerHTML = text;
+  return x;
 }
